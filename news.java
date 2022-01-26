4b47ca1a535407d1dee81355fb1f646455e4f4da
ATHI GURU <aathiharish1056@gmail.com>
2022-01-26 13:57:46 +0530

    firstcommit

---
+++ news.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class news  {
+    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+        StringBuilder sb=new StringBuilder("Hello");  
+    sb.delete(1,3);  
+    System.out.println(sb);//prints Hlo  
+
+    }
+
+
+    
+}
