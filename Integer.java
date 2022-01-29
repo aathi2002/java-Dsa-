dabfdee02935325bdc9fb85934c33453134145cb
ATHI GURU <aathiharish1056@gmail.com>
2022-01-29 19:06:17 +0530

    integer file has been changed

--- Integer.java
+++ Integer.java
@@ -6,24 +6,22 @@ public class Integer {
     public static void main(String[] args) {
         /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
         Scanner in=new Scanner(System.in);
-<<<<<<< HEAD
 		int N=in.nextInt();
 		int [] arr=new int [N];
 		for (int index = 0; index < arr.length; index++) {
 			arr[index]=in.nextInt();
 			
-=======
-		int test_case=in.nextInt();
-		while(test_case-->0){
-		    int a=in.nextInt();
-		    int b=in.nextInt();
-		    int ans=a-b;
-		    int ans3=Math.min(a,b);
-		    int final2=Math.min(ans3,ans);
-		    System.out.println(final2);
-			System.out.println(ans3);
->>>>>>> 2f1aba2fd4460528166cc3e998f02882d4525fd4
-		}
+
+		// int test_case=in.nextInt();
+		// while(test_case-->0){
+		//     int a=in.nextInt();
+		//     int b=in.nextInt();
+		//     int ans=a-b;
+		//     int ans3=Math.min(a,b);
+		//     int final2=Math.min(ans3,ans);
+		//     System.out.println(final2);
+		// 	System.out.println(ans3);
+		// }
 		HashSet<Integer>aathi=new HashSet<>();
 		for (int i = 0; i < arr.length; i++) {
 			aathi.add(arr[i]);
@@ -33,13 +31,12 @@ public class Integer {
 	
 		
     }
-<<<<<<< HEAD
 
 	
 
 	
 
 }
-=======
+
 }
->>>>>>> 2f1aba2fd4460528166cc3e998f02882d4525fd4
+
