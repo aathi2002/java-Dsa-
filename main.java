25735935ffd604e528a0f7e2d0a5fa20d5e47cd2
ATHI GURU <aathiharish1056@gmail.com>
2022-02-05 18:31:04 +0530

    first linkedlist

---
+++ main.java
@@ -0,0 +1,19 @@
+public class main {
+    public static void main(String[] args) {
+        ll list=new ll(); 
+        list.insertFirst(1);
+        list.insertFirst(2);
+        list.insertFirst(3);
+        list.insertFirst(4);
+        list .insertlast(94);
+        list.insert(66,4);
+        list.display();
+        // list.insertlast(93);
+        // list.display();
+
+      
+    
+
+    }
+    
+}
